774978f5075fee35384ea40a8846ce02d7b2b348
MichalDudekk <michaldudek.contact@gmail.com>
2025-08-08 20:38:01 +0200

    update

--- 19 - word break - rozbijanie słów.py
+++ 19 - word break - rozbijanie słów.py
@@ -59,24 +59,43 @@ def word_break_dict(words,s):
 
     return True if recur(0,n-1) == 1 else False
 
+# bottom up ze słownikiem, z inaczej zdefiniowaną funkcją - O(n^2)
+def word_break_best(words, s):
+    n = len(s)
+    words_dict = {}                     # Konwersja na słownik dla O(1) sprawdzeń
+    for word in words: words_dict[word] = True
+    F = [False for _ in range(n + 1)]   # F[i] = True jeśli s[0:i] da się podzielić
+    F[0] = True                         # Pusty ciąg jest zawsze "podzielny"
+
+    for i in range(1, n + 1):
+        for j in range(i):
+            if F[j] and s[j:i] in words_dict:
+                F[i] = True
+                break                   # Nie trzeba sprawdzać dalszych podziałów
+
+    return F[n]
+
 # Przykładowy test - oczekiwana wartość: True
 words = ["self", "th", "is", "famous", "Word", "break", "b", "r", "e", "a",
          "k", "br", "bre", "break", "ak", "problem"]
 s = "Wordbreakproblem"
 print(word_break(words,s))
 print(word_break_dict(words,s))
+print(word_break_best(words,s))
 # Przykładowy test - oczekiwana wartość: False
 words = ["self", "th", "is", "famous", "Word", "break", "b", "r", "e", "a",
          "k", "br", "bre", "break", "ak", "problem"]
 s = "Wordbrxakproblem"
 print(word_break(words,s))
 print(word_break_dict(words,s))
+print(word_break_best(words,s))
 # Przykładowy test - oczekiwana wartość: True
 words = ["self", "th", "is", "famous", "Word", "break", "b", "r", "e", "a",
          "k", "br", "bre", "break", "ak", "problem","x"]
 s = "Wordbrxakproblem"
 print(word_break(words,s))
 print(word_break_dict(words,s))
+print(word_break_best(words,s))
 
 # Opis algorytmu - word_break(words,s) - O(n^2 * (n + len(words)) )
 # recur(w,k) - czy ciąg znaków s[w:k+1] można podzielić na sekwencję słów z listy words
@@ -86,3 +105,9 @@ print(word_break_dict(words,s))
 # da się podzielić.
 # Liczba unikalnych podproblemów: O(n^2)
 # Rozwiązanie pojedyńczego podproblemu: O(n + len(words)) - przy użyciu słownika O(len(words)) redukuje się do O(1)
+
+# Opis algorytmu - word_break_best(words,s) - O(n^2)
+# F[i] - czy s[0:i] da się podzielić
+# Algorytm dla każdego wywołania pętli dzieli słowo s[0:i] na dwa s[0:j] i s[j:i].
+# To czy s[0:j] da się podzielić zostało już wcześniej obliczone i jest zapisane w F[j].
+# Pozostaje sprawdzić czy s[j:i] jest słowem w words, korzystając ze słownika możemy to zrobić w czasie stałym O(1).
