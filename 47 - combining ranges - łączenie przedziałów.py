1bb7e293af709dc2b0f3299cde1834a2cec3864e
MichalDudekk <michaldudek.contact@gmail.com>
2025-08-13 16:21:43 +0200

    Add files via upload

---
+++ 47 - combining ranges - łączenie przedziałów.py
@@ -0,0 +1,34 @@
+# 47 - combining ranges - łączenie przedziałów
+# Dany jest zbiór przedziałów [ai, bi]. Dwa przedziały mogą być połączone, jeśli mają
+# dokładnie jeden wspólny punkt. Znajdź algorytm, który sprawdza, czy można uzyskać
+# przedział [a, b] poprzez łączenie tych przedziałów.
+
+def ranges(T,a,b):
+    n = max(t[1] for t in T) + 1
+    G = [[] for _ in range(n)]
+    for x,y in T:
+        G[x].append(y)
+        G[y].append(x)
+
+    visited = [False]*n
+
+    def dfs_visit(v):
+        visited[v] = True
+        for u in G[v]:
+            if visited[u]:
+                continue
+            dfs_visit(u)
+
+    dfs_visit(a)
+    return visited[b]
+
+# Przykładowy test - oczekiwana wartość: True
+T = [[1, 3], [2, 3], [4, 5], [5, 7], [0, 6], [6, 7], [0, 2], [3, 4], [8, 9]]
+a = 0
+b = 1
+print(ranges(T,a,b))
+# Przykładowy test - oczekiwana wartość: False
+T = [[1, 3], [2, 3], [4, 5], [5, 7], [0, 6], [6, 7], [0, 2], [3, 4], [8, 9]]
+a = 5
+b = 9
+print(ranges(T,a,b))
