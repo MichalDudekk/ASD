17a88f1ecff20c70974be0432672759b0f43943e
MichalDudekk <michaldudek.contact@gmail.com>
2025-08-17 07:00:58 +0200

    Add files via upload

---
+++ 53 - packages delivery - dostarczanie paczek.py
@@ -0,0 +1,55 @@
+# 53 - packages delivery - dostarczanie paczek
+# Bitlandia to kraina zawierająca N miast i N-1 dróg dwukierunkowych. System dróg tworzy spójny
+# graf. Dana jest lista K miast, do których musimy dostarczyć paczki i mając możliwość
+# rozpoczęcia i zakończenia trasy w dowolnym mieście, znajdź minimalną odległość, którą
+# musimy pokonać, aby dostarczyć wszystkie paczki.
+
+def diameter(G):
+    n = len(G)
+    visited = [False]*n
+    res = 0
+
+    def dfs_visit(v):
+        visited[v] = True
+        max_dist_a = 0
+        max_dist_b = 0
+        for u in G[v]:
+            if visited[u]:
+                continue
+            dist = dfs_visit(u)
+            if dist > max_dist_b:
+                if dist > max_dist_a:
+                    max_dist_a, max_dist_b = dist, max_dist_a
+                else:
+                    max_dist_b = dist
+        nonlocal res
+        res = max(res, max_dist_a + max_dist_b )
+        return max_dist_a + 1
+    dfs_visit(0)
+    return res
+
+def delivery(G):
+    n = len(G)
+    visited = [False]*n
+    to_visit = n
+    res = 0
+
+    def dfs_visit(v):
+        nonlocal to_visit, res
+        visited[v] = True
+        to_visit -= 1
+
+        for u in G[v]:
+            if visited[u]:
+                continue
+            res += 1
+            dfs_visit(u)
+            res += 1
+
+    dfs_visit(0)
+    return res - diameter(G)
+
+# Przykładowy test - oczekiwana wartość: 27
+G = [[1], [0, 2], [1, 3, 4], [2, 6], [2, 5], [4], [3, 7, 8], [6], [6, 9, 10, 11],
+         [8], [8], [8, 12, 16], [11, 13, 14], [12, 15], [12], [13], [11, 17, 18], [16], [16]]
+print(delivery(G))
